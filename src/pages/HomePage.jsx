36b200e91ab5596291d4fc0d0b5c452bf5878e19
MeloScav <meloscavezzoni@gmail.com>
2022-12-21 21:05:11 +0100

    Init Home page + add layout component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "/src/components/layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <p>Hello from HomePage</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
