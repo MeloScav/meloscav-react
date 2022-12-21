3c40c9c2a85e443ee9b363776d8e10b97d180560
MeloScav <meloscavezzoni@gmail.com>
2022-12-21 21:02:06 +0100

    Add Provider + store

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import HomePage from "./pages/HomePage";
+import HomePage from "/src/pages/HomePage";
+import store from "/src/store";
 
 const App = (props) => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path='/' element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
