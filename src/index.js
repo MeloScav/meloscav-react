51ac643ef98394ac027c5389b0977d1e68e1f5b4
MeloScav <meloscavezzoni@gmail.com>
2023-01-08 17:55:07 +0100

    Check if content is loaded then remove transition class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import App from "./App";
+import App from "/src/App";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+// Check if content is loaded then remove transition class
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("body").classList.remove("remove-transition", "init-loading-pending");
+});
+
 root.render(<App />);
