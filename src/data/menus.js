319f22e4d196d679a525238bab714eb42bb988fd
MeloScav <meloscavezzoni@gmail.com>
2023-01-08 17:52:34 +0100

    Menu array

---
+++ src/data/menus.js
@@ -0,0 +1,23 @@
+const menus = [
+    {
+        to: "/category/design",
+        label: "Design",
+        iconId: "design-icon"
+    },
+    {
+        to: "/category/development",
+        label: "Développement",
+        iconId: "dvlpmt-icon"
+    },
+    {
+        to: "/category/illustration",
+        label: "Illustration",
+        iconId: "illu-icon"
+    },
+    {
+        href: "https://opensea.io/MeloScav?tab=created",
+        label: "NFTs",
+        isNew: true,
+    }
+];
+export default menus;
