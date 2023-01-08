e6a877bf98b799daf77a91f529d1cb1f2282f91f
MeloScav <meloscavezzoni@gmail.com>
2023-01-08 17:53:04 +0100

    Social media array

---
+++ src/data/socialMedia.js
@@ -0,0 +1,11 @@
+const socialMedia = [
+    {
+        link: "https://github.com/MeloScav",
+        label: "Github"
+    },
+    {
+        link: "https://www.linkedin.com/in/meloe-scavezzoni/",
+        label: "Linkedin",
+    }
+];
+export default socialMedia;
