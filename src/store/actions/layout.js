118f1be76bbc7704a4a3b7fc02600d009e88c3ef
MeloScav <meloscavezzoni@gmail.com>
2023-01-08 17:54:51 +0100

    Create functions to return actions with types

--- src/store/actions/layout.js
+++ src/store/actions/layout.js
@@ -1,11 +1,14 @@
 const prefix = "LAYOUT";
 
+// Create functions to return actions with types
 const layoutActions = {
     MENU_TRANSPARENT: `${prefix}_MENU_TRANSPARENT`,
     MENU_OPAQUE: `${prefix}_MENU_OPAQUE`,
     MENU_MOBILE_OPEN: `${prefix}_MENU_MOBILE_OPEN`,
     MENU_MOBILE_CLOSE: `${prefix}_MENU_MOBILE_CLOSE`,
     MENU_MOBILE_TOGGLE: `${prefix}_MENU_MOBILE_TOGGLE`,
+    LOADING_START: `${prefix}_LOADING_START`,
+    LOADING_FINISH: `${prefix}_LOADING_FINISH`,
 
     doMenuTransparent: () => {
         return {
@@ -34,6 +37,18 @@ const layoutActions = {
     doToggleMobileMenu: () => {
         return {
             type: layoutActions.MENU_MOBILE_TOGGLE
+        }
+    },
+
+    doStartLoading: () => {
+        return {
+            type: layoutActions.LOADING_START
+        }
+    },
+
+    doFinishLoading: () => {
+        return {
+            type: layoutActions.LOADING_FINISH
         }
     }
 };
