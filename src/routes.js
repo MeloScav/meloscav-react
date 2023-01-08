18dfc8b4e33328281df8ea67c703666434182f03
MeloScav <meloscavezzoni@gmail.com>
2023-01-08 17:55:16 +0100

    Routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 const routes = [
   {
     path: "/",
-    loader: () => import("/src/pages/HomePage"),
+    loader: () => import("/src/pages/homepage/HomePage"),
+  },
+  {
+    path: "/category/:categoryId",
+    loader: () => import("/src/pages/category/Category"),
+  },
+  {
+    path: "/projects/:projectId",
+    loader: () => import("/src/pages/project/Project"),
+  },
+  {
+    path: "*",
+    loader: () => import("/src/pages/NotFound"),
   },
 ];
 
